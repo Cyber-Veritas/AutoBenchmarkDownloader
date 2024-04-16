790dc253fcf4c58c59fc0e57c1c09ed5ff359a10
wgajuraj <108196707+wgajuraj@users.noreply.github.com>
2024-04-16 15:03:37 +0200

    basic download system

--- AutoBenchmarkDownloader/MainWindow.xaml.cs
+++ AutoBenchmarkDownloader/MainWindow.xaml.cs
@@ -1,43 +1,17 @@
-﻿using AutoBenchmarkDownloader.Model;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using System.IO;
+using System.Net.Http;
 using System.Windows;
+using AutoBenchmarkDownloader.ViewModel;
 
 namespace AutoBenchmarkDownloader
 {
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            DataContext = this;
             InitializeComponent();
-            SoftwareInfos =
-            [
-                new SoftwareInfo { Name="Software A", Address="https://example.com/", Download=true },
-                new SoftwareInfo { Name="Software B", Address="https://example.com/", Download=false }
-            ];
+            MainWindowViewModel vm = new MainWindowViewModel();
+            DataContext = vm;
         }
-
-        private ObservableCollection<SoftwareInfo> _softwareInfos;
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        public ObservableCollection<SoftwareInfo> SoftwareInfos
-        {
-            get { return _softwareInfos; }
-            set 
-            { 
-                _softwareInfos = value;
-                OnPropertyChanged("SoftwareInfos");
-                
-            }
-        }
-
-        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
     }
 }
