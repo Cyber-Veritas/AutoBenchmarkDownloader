3ebbde7cc03ee56b53119e3efdbebdb1ed2007a9
wgajuraj <108196707+wgajuraj@users.noreply.github.com>
2024-04-26 15:11:57 +0200

    branch merge fix

--- AutoBenchmarkDownloader/ViewModel/MainWindowViewModel.cs
+++ AutoBenchmarkDownloader/ViewModel/MainWindowViewModel.cs
@@ -1,18 +1,9 @@
-using AutoBenchmarkDownloader.MVVM;
-using System.ComponentModel;
-
 namespace AutoBenchmarkDownloader.ViewModel
 {
     internal class MainWindowViewModel
     {
-        public DxDiagInfoViewModel DxDiagInfoViewModel { get; }
-        public SystemUsageInfoViewModel SystemUsageInfoView { get; }
-
-        public MainWindowViewModel  ()
-        {
-            DxDiagInfoViewModel = new DxDiagInfoViewModel ();
-            SystemUsageInfoView = new SystemUsageInfoViewModel ();
-        }
-
+        public SoftwareInfoViewModel SoftwareInfoVm { get; } = new();
+        public DxDiagInfoViewModel DxDiagInfoVm { get; } = new();
+        public SystemUsageInfoViewModel SystemUsageInfoVm { get; } = new();
     }
 }
